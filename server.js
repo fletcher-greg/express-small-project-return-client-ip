2f9c3a132a81ab4328859d5b8d746ef95f5f6c3b
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-28 00:40:23 +0100

    cleanup

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const helmet = require("helmet");
 //  init app
 
 const app = express();
+
 app.set("views", "public");
 app.set("view engine", "pug");
 app.route("/").get((req, res) => {
-  let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
-  //   let ip = req.ips;
-  console.log(req.ips);
-  console.log(req.ip);
-  res.render("index", { ip: req.ip });
+  //   let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
+  let ip = req.ip;
+  res.setHeader("X-Powered-By", "Awesomeness");
+  res.render("index", { ip: ip });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
