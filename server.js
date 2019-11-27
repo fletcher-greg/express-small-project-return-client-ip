0a6ad1ef37623b5a66ab96df6d1485f658a8c6b0
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-28 00:15:59 +0100

    basic functionality plus simple styling

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const helmet = require("helmet");
 //  init app
 
 const app = express();
-
-app.set("trust proxy", true);
+app.set("views", "public");
+app.set("view engine", "pug");
 app.route("/").get((req, res) => {
   let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   //   let ip = req.ips;
   console.log(req.ips);
   console.log(req.ip);
-  res.send(`Your ip address is ${ip}`);
+  res.render("index", { ip: req.ip });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
