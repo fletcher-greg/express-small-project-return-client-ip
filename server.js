c9e7564e062596c01ec0056482eac02aa551ae5c
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-11-27 23:46:20 +0100

    add index.html

--- server.js
+++ server.js
@@ -4,5 +4,13 @@ const helmet = require("helmet");
 
 const app = express();
 
+app.set("trust proxy", true);
+app.route("/").get((req, res) => {
+  let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
+  //   let ip = req.ips;
+  console.log(req.ips);
+  console.log(req.ip);
+  res.send(`Your ip address is ${ip}`);
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
